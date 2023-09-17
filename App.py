58a50381d004e452f11864a68076f3f2d73b6219
DaMoAndes <d.moralesa2@uniandes.edu.co>
2023-09-17 10:14:39 -0500

    run main v2

--- App.py
+++ App.py
@@ -3,9 +3,12 @@ from flask_restful import Api
 from vistas.AppLogic import HealthCheck, Prueba
 
 app = Flask(__name__)
-api = Api(app)
-api.add_resource(Prueba, '/Cuestionario')
-api.add_resource(HealthCheck, '/HealthCheck')
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+#api = Api(app)
+#api.add_resource(Prueba, '/Cuestionario')
+#api.add_resource(HealthCheck, '/HealthCheck')
 
 if __name__ == "__main__":
     app.run()
