e6273013677f34e47c8035a91f220652a96da59f
MafeGarciaM <mf.garciam1@uniandes.edu.co>
2023-10-22 21:57:17 -0500

    Conexión con back para registro

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -5,11 +6,11 @@ import { AppComponent } from './app.component';
 import { SinginComponent } from './Usuarios/singin/singin.component';
 import { HomeComponent } from './Usuarios/home/home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginEmpresaComponent } from './Empresas/login-empresa/login-empresa.component';
-import { LoginCandidatoComponent } from './Candidatos/login-candidato/login-candidato.component';
 import { MenuComponent } from './Menu/menu.component';
 import { RegistroLaboralComponent } from './Candidatos/registro-laboral/registro-laboral.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginEmpresaComponent } from './Empresas/login-empresa/login-empresa.component';
+import { LoginCandidatoComponent } from './Candidatos/login-candidato/login-candidato.component';
 import { VistaEquipoTrabajoComponent } from './Empresas/EquipoDeTrabajo/vista-equipo-trabajo/vista-equipo-trabajo.component';
 import { CrearEquipoTrabajoComponent } from './Empresas/EquipoDeTrabajo/crear-equipo-trabajo/crear-equipo-trabajo.component';
 
@@ -31,6 +32,7 @@ import { CrearEquipoTrabajoComponent } from './Empresas/EquipoDeTrabajo/crear-eq
     FormsModule,
     ReactiveFormsModule,
     NgbModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
