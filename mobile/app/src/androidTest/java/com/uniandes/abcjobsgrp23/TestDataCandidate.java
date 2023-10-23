ba7b3e96ccd0bb4828fffda9592babb810821761
LuisCruz <lf.cruz2@uniandes.edu.co>
2023-10-23 00:24:43 -0500

    feat(test candidatos): loguin y register

---
+++ mobile/app/src/androidTest/java/com/uniandes/abcjobsgrp23/TestDataCandidate.java
@@ -0,0 +1,26 @@
+package com.uniandes.abcjobsgrp23;
+
+public class TestDataCandidate {
+    public static final String NOMBRE = "Nombre";
+    public static final String EDAD = "25";
+    public static final String TELEFONO = "123456789";
+    public static final String EMAIL = "correo@example.com";
+    public static final String PAIS = "País";
+    public static final String CIUDAD = "Ciudad";
+    public static final String IDIOMAS = "Ingles, Aleman";
+    public static final String RASGOS_PERSONALES = "Responsable";
+    public static final String PASSWORD = "password";
+    public static final String COLOMBIA ="Colombia";
+
+    public static final String NAME_HINT = "Nombre del aspirante*";
+    public static final String AGE_HINT = "Edad*";
+    public static final String PHONE_HINT = "Teléfono*";
+    public static final String EMAIL_HINT = "Correo Electrónico*";
+    public static final String COUNTRY_HINT = "País*";
+    public static final String CITY_HINT = "Ciudad*";
+    public static final String LANGUAGES_HINT = "Idiomas*";
+    public static final String TRAITS_HINT = "Rasgos Personales*";
+    public static final String PASSWORD_HINT = "Contraseña*";
+    public static final String CONFIRM_PASSWORD_HINT = "Confirmar Contraseña*";
+
+}
