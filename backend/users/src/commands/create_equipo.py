fb783bc4246f458e69b4f0947193ebd226bfe9c5
DaMoAndes <d.moralesa2@uniandes.edu.co>
2023-10-26 21:18:57 -0500

    Creación blueprint, BaseCommand, arreglo modelo empresa, creación modelo equipo

---
+++ backend/users/src/commands/create_equipo.py
@@ -0,0 +1,39 @@
+from .base_command import BaseCommannd
+from ..models.user import User, UserSchema, CreatedUserJsonSchema
+from ..models.equipo import Equipo, EquipoSchema, EmpresaJsonSchema, CreatedEmpresaJsonSchema
+from ..models.empresa import EmpresaSchema, Empresa
+from ..session import Session
+
+
+class CreateEquipo(BaseCommannd):
+    try:
+        def __init__(self, data):
+            self.data = data
+        
+        def execute(self):
+            empresa_data = {
+                "tipoEmpresa": self.data.pop("tipoEmpresa"),
+                "razonSocial":self.data.pop("razonSocial"),
+                "verticalesNegocio":self.data.pop("verticalesNegocio")
+
+            }
+            
+            posted_empresa = EmpresaSchema(only=("tipoEmpresa","razonSocial","verticalesNegocio")).load(
+                empresa_data
+            )
+            empresa = Empresa(**posted_empresa)
+            posted_equipo = EquipoSchema(only=("nombre", "descripcion","empleados")).load(self.data)
+            equipo = Equipo(**posted_equipo, idEmpresa=empresa.id)
+            session = Session()
+
+            session.add(empresa)
+            session.add(equipo)
+            session.commit()
+            new_empresa = CreatedEmpresaJsonSchema().dump(empresa)
+            session.close()
+            return new_empresa
+    except Exception as error:
+        # handle the exception
+        print("An exception occurred:", error) # An exception occurred: division by zero
+        
+
