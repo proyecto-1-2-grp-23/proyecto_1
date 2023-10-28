1726978e3f16a48cfaf2fbe5f2d439807443237f
DaMoAndes <d.moralesa2@uniandes.edu.co>
2023-10-27 21:59:10 -0500

    Se crea endpoint, base_command, blueprints

--- backend/users/src/commands/create_equipo.py
+++ backend/users/src/commands/create_equipo.py
@@ -3,7 +3,7 @@ from ..models.user import User, UserSchema, CreatedUserJsonSchema
 from ..models.equipo import Equipo, EquipoSchema, EmpresaJsonSchema, CreatedEmpresaJsonSchema
 from ..models.empresa import EmpresaSchema, Empresa
 from ..session import Session
-
+from flask import Flask, jsonify
 
 class CreateEquipo(BaseCommannd):
     try:
@@ -32,6 +32,10 @@ class CreateEquipo(BaseCommannd):
             new_empresa = CreatedEmpresaJsonSchema().dump(empresa)
             session.close()
             return new_empresa
+        
+        def correo_exist(self, session):
+            return len(session.query(User).filter_by().all()) > 0
+
     except Exception as error:
         # handle the exception
         print("An exception occurred:", error) # An exception occurred: division by zero
