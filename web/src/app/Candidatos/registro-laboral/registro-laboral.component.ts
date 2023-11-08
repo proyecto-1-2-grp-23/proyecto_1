a82979cb7c13797a535dcc8a109a6ab623ca81c4
DaMoAndes <d.moralesa2@uniandes.edu.co>
2023-11-07 23:58:30 -0500

    Fix: registro laboral de un usuario aspirante

--- web/src/app/Candidatos/registro-laboral/registro-laboral.component.ts
+++ web/src/app/Candidatos/registro-laboral/registro-laboral.component.ts
@@ -2,6 +2,11 @@ import { Router } from '@angular/router';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { NgbCalendar, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { ServicioRegistroLaboralService } from '../servicio/servicio-registro-laboral.service';
+import { candidatoRegistroLaboral } from '../laboral';
+import Swal from 'sweetalert2';
+import { candidatoSignIn } from 'src/app/Usuarios/usuario';
+
 
 @Component({
   selector: 'app-registro-laboral',
@@ -11,6 +16,7 @@ import { NgbCalendar, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 export class RegistroLaboralComponent implements OnInit {
   fechaInicioSeleccionada!: NgbDateStruct;
   fechaFinSeleccionada!: NgbDateStruct;
+  laboral!: candidatoRegistroLaboral;
 
   chips: string[] = [];
   nuevoChip: string = '';
@@ -21,11 +27,14 @@ export class RegistroLaboralComponent implements OnInit {
     funciones: new FormControl('', [Validators.required]),
     fechaInicio: new FormControl('', [Validators.required]),
     fechaFin: new FormControl('', [Validators.required]),
+    habilidades: new FormControl('', [Validators.required])
   });
 
-  constructor(private calendar: NgbCalendar, private router: Router) {}
+  constructor(private calendar: NgbCalendar,
+    private router: Router,
+    private laboralService: ServicioRegistroLaboralService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   abrirDatepicker() {
     // Inicializa la fecha con el valor actual
@@ -49,6 +58,33 @@ export class RegistroLaboralComponent implements OnInit {
   }
 
   guardar() {
-    this.router.navigate([`/menu/candidato`]);
+    this.laboral = {
+      nombre_empresa: this.registrationForm.get('nombreEmpresa')?.value,
+      rol: this.registrationForm.get('rol')?.value,
+      funciones: this.registrationForm.get('funciones')?.value.toString(),
+      fecha_inicio: formatDate(this.registrationForm.get('fechaInicio')?.value),
+      fecha_fin: formatDate(this.registrationForm.get('fechaFin')?.value),
+      habilidades: this.registrationForm.get('habilidades')?.value,
+    };
+    function formatDate(date: { year: any; month: any; day: any; }) {
+      const year = date.year;
+      const month = String(date.month).padStart(2, '0');
+      const day = String(date.day).padStart(2, '0');
+      return `${year}-${month}-${day}`;
+    }
+    console.log(this.laboral)
+    this.laboralService.candidatoRegistroLaboral(this.laboral)
+      .subscribe((res) => {
+        if (res.id > 0) {
+          Swal.fire('', 'Candidato registrado', 'success');
+          this.router.navigate([`/signin/candidato`]);
+        } else {
+          Swal.fire('', 'Error en el registro', 'error');
+        }
+      });
+
   }
+
+  //this.router.navigate([`/menu/candidato`]);
+
 }
