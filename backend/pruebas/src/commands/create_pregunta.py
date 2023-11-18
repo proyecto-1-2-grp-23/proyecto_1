133c80c64f94a5ce62f42860210a04aaf4c48af7
Daniel Babativa <da.babativa@uniandes.edu.co>
2023-11-18 15:57:22 -0500

    removed try except

--- backend/pruebas/src/commands/create_pregunta.py
+++ backend/pruebas/src/commands/create_pregunta.py
@@ -5,39 +5,31 @@ from ..session import Session
 
 
 class CreatePregunta(BaseCommannd):
-    try:
+    def __init__(self, data):
+        self.data = data
 
-        def __init__(self, data):
-            self.data = data
+    def execute(self):
+        body: dict = self.data.copy()
+        respuestas = body.pop("respuestas")
 
-        def execute(self):
-            body: dict = self.data.copy()
-            respuestas = body.pop("respuestas")
+        posted_data = PreguntaSchema(
+            only=(
+                "idProyecto",
+                "dificultad",
+                "descripcion",
+            )
+        ).load(body)
 
-            posted_data = PreguntaSchema(
-                only=(
-                    "idProyecto",
-                    "dificultad",
-                    "descripcion",
-                )
-            ).load(body)
+        pregunta = Pregunta(**posted_data)
+        session = Session()
+        session.add(pregunta)
+        session.commit()
+        for respuesta in respuestas:
+            posted_data = RespuestaSchema(only=("descripcion",)).load(respuesta)
+            respuesta_db = Respuesta(**posted_data, idPregunta=pregunta.id)
+            session.add(respuesta_db)
+        session.commit()
+        new_pregunta = CreatedPreguntaJsonSchema().dump(pregunta)
+        session.close()
 
-            pregunta = Pregunta(**posted_data)
-            session = Session()
-            session.add(pregunta)
-            session.commit()
-            for respuesta in respuestas:
-                posted_data = RespuestaSchema(only=("descripcion",)).load(respuesta)
-                respuesta_db = Respuesta(**posted_data, idPregunta=pregunta.id)
-                session.add(respuesta_db)
-            session.commit()
-            new_pregunta = CreatedPreguntaJsonSchema().dump(pregunta)
-            session.close()
-
-            return new_pregunta
-
-    except Exception as error:
-        # handle the exception
-        print(
-            "An exception occurred:", error
-        )  # An exception occurred: division by zero
+        return new_pregunta
